5460d6a11c1ab6fb8688d594113e2166fa550266
Sven Flossmann <sven.flossmann@gmail.com>
2019-05-17 18:33:44 +0200

    ADDED: Initial class Participant

---
+++ DSVAlpin2Lib/Participant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSVAlpin2Lib
+{
+  class Participant
+  {
+    public string Name { get; set; }
+    public string Firstname { get; set; }
+    public int Year { get; set; }
+    public string Club{ get; set; }
+  }
+}
