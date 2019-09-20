9897a5eb5ed3ec6d12fb2b56d85d6deca36d362c
Sven Flossmann <sven.flossmann@gmail.com>
2019-09-20 22:18:41 +0200

    ADDED: ObservableCollection.InsertRange()
    Inserts several objects without separate notification

--- DSVAlpin2Lib/Utilities.cs
+++ DSVAlpin2Lib/Utilities.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -139,8 +140,8 @@ namespace DSVAlpin2Lib
 
   public class CopyObservableCollection<T> : ObservableCollection<T>
   {
-    ObservableCollection<T> _source;
-    Cloner<T> _cloner;
+    protected ObservableCollection<T> _source;
+    protected Cloner<T> _cloner;
 
     public delegate TC Cloner<TC>(TC source);
     public CopyObservableCollection(ObservableCollection<T> source, Cloner<T> cloner)
@@ -159,7 +160,7 @@ namespace DSVAlpin2Lib
     }
 
 
-    private void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+    protected void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
       // Clone
       throw new NotImplementedException();
@@ -264,6 +265,34 @@ namespace DSVAlpin2Lib
       }
     }
 
+
+    public static void InsertRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+      var enumerable = items as List<T> ?? items.ToList();
+      if (collection == null || items == null || !enumerable.Any())
+      {
+        return;
+      }
+
+      Type type = collection.GetType();
+
+      type.InvokeMember("CheckReentrancy", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null, collection, null);
+      var itemsProp = type.BaseType.GetProperty("Items", BindingFlags.NonPublic | BindingFlags.FlattenHierarchy | BindingFlags.Instance);
+      var privateItems = itemsProp.GetValue(collection) as IList<T>;
+      foreach (var item in enumerable)
+      {
+        privateItems.Add(item);
+      }
+
+      type.InvokeMember("OnPropertyChanged", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null,
+        collection, new object[] { new PropertyChangedEventArgs("Count") });
+
+      type.InvokeMember("OnPropertyChanged", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null,
+        collection, new object[] { new PropertyChangedEventArgs("Item[]") });
+
+      type.InvokeMember("OnCollectionChanged", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null,
+        collection, new object[] { new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset) });
+    }
 
   }
 
