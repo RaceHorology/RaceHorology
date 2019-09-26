930e9eefa1020c11e7f2958b4fc364aa0485f41d
Sven Flossmann <sven.flossmann@gmail.com>
2019-09-26 20:48:00 +0200

    ADDED: Extension method ComboBox.SelectCBItem()

--- DSVAlpin2/UIUtilities.cs
+++ DSVAlpin2/UIUtilities.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace DSVAlpin2
 {
   public class CBItem
   {
     public string Text { get; set; }
-    public string Value { get; set; }
+    public object Value { get; set; }
 
     public override string ToString()
     {
       return Text;
     }
   }
+
+  public static class CBItemExtensions
+  {
+
+    public static bool SelectCBItem(this ComboBox cmb, object value)
+    {
+      foreach (CBItem item in cmb.Items)
+        if (object.Equals(item.Value, value))
+        {
+          cmb.SelectedValue = item;
+          return true;
+        }
+
+      return false;
+    }
+
+  }
+
 
 }
