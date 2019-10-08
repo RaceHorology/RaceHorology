f0c26d10c48ec9b5c0709cccc380b23a53d4191e
Sven Flossmann <sven.flossmann@gmail.com>
2019-10-08 22:04:02 +0200

    ADDED: LiveTiming DLL for rennmeldung.de

---
+++ LiveTimingRM/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LiveTimingRM
+{
+
+  public class Class1
+  {
+
+    public Class1()
+    {
+    }
+
+    public void Login()
+    {
+      rmlt.LiveTiming lv = new rmlt.LiveTiming();
+      lv.LoginLiveTiming("01122", "livetiming", "livetiming");
+
+
+    }
+  }
+}
