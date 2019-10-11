678488f827df15c7b7fd373f428fc22de05d45d5
Sven Flossmann <sven.flossmann@gmail.com>
2019-10-11 20:25:36 +0200

    CHANGED: Using the new DLL from 2019-10-11

--- LiveTimingRM/Class1.cs
+++ LiveTimingRM/Class1.cs
@@ -13,7 +13,7 @@ namespace LiveTimingRM
     public void Login()
     {
       rmlt.LiveTiming lv = new rmlt.LiveTiming();
-      lv.LoginLiveTiming("01122", "livetiming", "livetiming");
+      lv.LoginLiveTiming("01122", "livetiming", "livetiming", @"C:\src\DSVAlpin2\work\DSVAlpin2\LiveTimingRM\3rdparty\DSValpinX.liz");
 
 
     }
