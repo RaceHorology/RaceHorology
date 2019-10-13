8f5d8ce0b4b99bb7e9b33dbcc7d53c9c1fca59b1
Sven Flossmann <sven.flossmann@gmail.com>
2019-10-13 20:18:02 +0200

    ADDED: iText + First Report

---
+++ DSVAlpin2Lib/PDFReports.cs
@@ -0,0 +1,24 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSVAlpin2Lib
+{
+  public class PDFReport1
+  {
+    public static void Test()
+    {
+
+      var writer = new PdfWriter("test.pdf");
+      var pdf = new PdfDocument(writer);
+      var document = new Document(pdf);
+      document.Add(new Paragraph("Hello World!"));
+      document.Close();
+    }
+  }
+}
