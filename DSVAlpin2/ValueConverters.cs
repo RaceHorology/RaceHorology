f99fd9fe88e83cd74d4036df4f4c730e8a46a994
Sven Flossmann <sven.flossmann@gmail.com>
2019-11-09 14:01:11 +0100

    REFACTORED: Moving value converters to DSVAlpin2Lib
    Reason: Shall be used for PDF reports

--- DSVAlpin2/ValueConverters.cs
+++ DSVAlpin2/ValueConverters.cs
@@ -10,26 +10,6 @@ using System.Windows.Media;
 
 namespace DSVAlpin2
 {
-  /// <summary>
-  /// Converts a position number into a string. Position 0 is transferred into "./."
-  /// </summary>
-  public class PositionConverter : IValueConverter
-  {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-      uint position = (uint)value;
-
-      if (position == 0)
-        return "./.";
-
-      return position.ToString();
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-      throw new NotImplementedException();
-    }
-  }
 
   public class BooleanToBrushConverter : IValueConverter
   {
@@ -63,63 +43,5 @@ namespace DSVAlpin2
     }
   }
 
-
-  public class ResultCodeConverter : IValueConverter
-  {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-      try
-      {
-        RunResult.EResultCode rc = (RunResult.EResultCode)value;
-
-        if (rc == RunResult.EResultCode.Normal)
-          return "";
-
-        return rc.ToString();
-      }
-      catch (Exception)
-      {
-        return "";
-      }
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-      throw new NotImplementedException();
-    }
-  }
-
-
-  public class ResultCodeWithCommentConverter : IMultiValueConverter
-  {
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-    {
-      if (values.Length != 2)
-        throw new Exception("invalid number of argumnets");
-
-      try
-      {
-        RunResult.EResultCode rc = (RunResult.EResultCode)values[0];
-        string comment = (string)values[1];
-
-        if (rc == RunResult.EResultCode.Normal)
-          return "";
-
-        if (string.IsNullOrEmpty(comment))
-          return rc.ToString();
-        else
-          return string.Format("{0} ({1})", rc.ToString(), comment);
-      }
-      catch(Exception)
-      {
-        return "";
-      }
-    }
-
-    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-    {
-      throw new NotImplementedException();
-    }
-  }
 
 }
