46f0235ed8bfd41d982b4211e4f9d5c190873b29
Sven Flossmann <sven.flossmann@gmail.com>
2019-11-17 21:34:58 +0100

    CHANGED: Further cosmetics (correct axis, ...)

--- DSVAlpin2/ChartTestsUC.xaml.cs
+++ DSVAlpin2/ChartTestsUC.xaml.cs
@@ -45,70 +45,126 @@ namespace DSVAlpin2
 
     public void Display(RaceResultViewProvider results)
     {
-      DisplayMSCHart(results);
+      SetupChart(msChart.GetChart(), results);
     }
 
 
-    public void DisplayMSCHart(RaceResultViewProvider results)
+    public void configureAxis(ChartArea area, RaceResultViewProvider results)
     {
+      area.AxisX.Minimum = 0.5; // Double.NaN;
+      area.AxisX.Maximum = Double.NaN;
 
-      msChart.GetChart().Series.Clear();
-
-      if (msChart.GetChart().ChartAreas.Count() == 0)
+      area.AxisX.CustomLabels.Clear();
+      var lr = results.GetView() as System.Windows.Data.ListCollectionView;
+      if (lr.Groups != null)
       {
-        ChartArea area = new ChartArea();
-        area.AxisX.Minimum = Double.NaN;
-        area.AxisX.Maximum = Double.NaN;
+        int x1 = 1;
+        int x2Last = 1;
+        int x2 = 1;
+        string name2=null, name2Last=null;
+        foreach (var group in lr.Groups)
+        {
+          if (group is System.Windows.Data.CollectionViewGroup cvGroup)
+          {
+            var lblGroup = area.AxisX.CustomLabels.Add( x1 - 0.5, x1 + 0.5, cvGroup.Name.ToString());
+            lblGroup.GridTicks = GridTickTypes.None;
 
-        var element = area.AxisX.CustomLabels.Add(0.5, 1.5, "ABC");
-        element.GridTicks = GridTickTypes.All;
-        element = area.AxisX.CustomLabels.Add(1.5, 2.5, "DEF");
-        element.GridTicks = GridTickTypes.TickMark;
+            // Second Level if possible
+            if (cvGroup.Name is ParticipantClass @class)
+            {
+              name2 = @class.Group.Name;
+              if (name2Last == null) // init set to same name
+                name2Last = name2;
 
-        element = area.AxisX.CustomLabels.Add(2.5, 3.5, "GEH");
-        element.GridTicks = GridTickTypes.TickMark;
-        element = area.AxisX.CustomLabels.Add(3.5, 4.5, "JKL");
-        element.GridTicks = GridTickTypes.TickMark;
+              if (!string.Equals(name2, name2Last))
+              {
+                var lblName2 = area.AxisX.CustomLabels.Add( x2Last - 0.5, (x2-1) + 0.5, name2Last, 1, LabelMarkStyle.LineSideMark);
+                name2Last = name2;
+                x2Last = x2;
+              }
+            }
+          }
 
-        element = area.AxisX.CustomLabels.Add(0.5, 2.5, "G1", 1, LabelMarkStyle.LineSideMark);
-        element = area.AxisX.CustomLabels.Add(2.5, 4.5, "G2", 1, LabelMarkStyle.LineSideMark);
+          x1++;
+          x2++;
+        }
 
-        area.AxisY.Minimum = Double.NaN;
-        area.AxisY.Maximum = Double.NaN;
-        area.AxisY.IsMarginVisible = false;
-        area.AxisY.IsStartedFromZero = false;
-        area.AxisY.Title = "Zeit";
-
-
-        // Enable scale breaks
-        area.AxisY.ScaleBreakStyle.Enabled = true;
-
-        // Set the scale break type
-        area.AxisY.ScaleBreakStyle.BreakLineStyle = BreakLineStyle.Wave;
-
-        // Set the spacing gap between the lines of the scale break (as a percentage of y-axis)
-        area.AxisY.ScaleBreakStyle.Spacing = 2;
-
-        // Set the line width of the scale break
-        area.AxisY.ScaleBreakStyle.LineWidth = 2;
-
-        // Set the color of the scale break
-        area.AxisY.ScaleBreakStyle.LineColor = System.Drawing.Color.Red;
-
-        // Show scale break if more than 25% of the chart is empty space
-        area.AxisY.ScaleBreakStyle.CollapsibleSpaceThreshold = 25;
-
-        // If all data points are significantly far from zero, 
-        // the Chart will calculate the scale minimum value
-        area.AxisY.ScaleBreakStyle.StartFromZero = StartFromZero.Yes;
-
-        msChart.GetChart().ChartAreas.Add(area);
-
-        //msChart.GetChart().SaveImage("", ChartImageFormat.)
+        // Final 2nd group
+        if (name2 != null && name2Last != null)
+        {
+          var lblName2 = area.AxisX.CustomLabels.Add(x2Last - 0.5, (x2-1)+ 0.5, name2Last, 1, LabelMarkStyle.LineSideMark);
+        }
       }
 
-      var ds = new System.Windows.Forms.DataVisualization.Charting.Series();
+      // Enable X axis labels automatic fitting
+      area.AxisX.IsLabelAutoFit = true;
+      area.AxisX.LabelAutoFitStyle = LabelAutoFitStyles.DecreaseFont | LabelAutoFitStyles.IncreaseFont | LabelAutoFitStyles.WordWrap;
+      area.AxisX.MajorGrid.Enabled = false;
+      area.AxisX.MinorGrid.Enabled = false;
 
+      area.AxisY.Minimum = Double.NaN;
+      area.AxisY.Maximum = Double.NaN;
+      area.AxisY.IsMarginVisible = false;
+      area.AxisY.IsStartedFromZero = false;
+      area.AxisY.Title = "Zeit";
+
+      area.AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Solid;
+      area.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+      area.AxisY.MajorGrid.Interval = 2.0;
+
+      area.AxisY.MinorGrid.Enabled = true;
+      area.AxisY.MinorGrid.LineDashStyle = ChartDashStyle.Dot;
+      area.AxisY.MinorGrid.LineColor = System.Drawing.Color.LightGray;
+      area.AxisY.MinorGrid.Interval = 0.5;
+
+
+      // Enable scale breaks
+      area.AxisY.ScaleBreakStyle.Enabled = true;
+      area.AxisY.ScaleBreakStyle.BreakLineStyle = BreakLineStyle.Wave;
+      area.AxisY.ScaleBreakStyle.Spacing = 2;
+      area.AxisY.ScaleBreakStyle.LineWidth = 2;
+      area.AxisY.ScaleBreakStyle.LineColor = System.Drawing.Color.Red;
+      area.AxisY.ScaleBreakStyle.CollapsibleSpaceThreshold = 25;
+      // If all data points are significantly far from zero, the Chart will calculate the scale minimum value
+      area.AxisY.ScaleBreakStyle.StartFromZero = StartFromZero.Yes;
+    }
+
+
+    public void addResult(System.Windows.Forms.DataVisualization.Charting.Series ds, object result, int x)
+    {
+      if (!(result is RaceResultItem item))
+        return;
+
+      double timeValue = 0.0;
+      if (item.TotalTime != null)
+        timeValue = ((TimeSpan)item.TotalTime).TotalMilliseconds / 1000.0;
+
+      if (timeValue > 0.0)
+      {
+        DataPoint p = new DataPoint(x, timeValue);
+        //DataPoint p = new DataPoint(timeValue, x);
+        //p.Label = item.Participant.Fullname;
+        p.Label = string.Format("{0} {1}.", item.Participant.Name, item.Participant.Firstname.Substring(0,1));
+
+        ds.Points.Add(p);
+      }
+    }
+
+
+    protected void createAndAddChartArea(Chart chart, RaceResultViewProvider results)
+    {
+      if (chart.ChartAreas.Count() == 0)
+      {
+        ChartArea area = new ChartArea();
+        chart.ChartAreas.Add(area);
+      }
+
+      configureAxis(chart.ChartAreas[0], results);
+    }
+
+
+    protected void fillDataSeries(System.Windows.Forms.DataVisualization.Charting.Series ds, RaceResultViewProvider results)
+    {
       // Populate series data with random data
       var lr = results.GetView() as System.Windows.Data.ListCollectionView;
       if (lr.Groups != null)
@@ -116,49 +172,56 @@ namespace DSVAlpin2
         int x = 1;
         foreach (var group in lr.Groups)
         {
-          System.Windows.Data.CollectionViewGroup cvGroup = group as System.Windows.Data.CollectionViewGroup;
-
-          foreach (var result in cvGroup.Items)
+          if (group is System.Windows.Data.CollectionViewGroup cvGroup)
           {
-            if (!(result is RaceResultItem item))
-              return;
+            foreach (var result in cvGroup.Items)
+              addResult(ds, result, x);
 
-            double timeValue = 0.0;
-            if (item.TotalTime != null)
-              timeValue = ((TimeSpan)item.TotalTime).TotalMilliseconds / 1000.0;
-
-            if (timeValue > 0.0)
-            {
-              DataPoint p = new DataPoint(x, timeValue);
-              p.Label = item.Participant.Fullname;
-
-              ds.Points.Add(p);
-              //ds.Points.AddXY(x, timeValue);
-            }
+            x++;
           }
-
-          x++;
         }
       }
+    }
 
+
+    protected void formatDataSeries(System.Windows.Forms.DataVisualization.Charting.Series ds, RaceResultViewProvider results)
+    {
       // Set point chart type
       ds.ChartType = SeriesChartType.Point;
 
       // Enable data points labels
       ds.IsValueShownAsLabel = true;
-      ds["LabelStyle"] = "Center";
+      ds["LabelStyle"] = "Bottom";
+
+      ds.SmartLabelStyle.Enabled = true;
+      ds.SmartLabelStyle.AllowOutsidePlotArea = LabelOutsidePlotAreaStyle.Partial;
+      ds.SmartLabelStyle.CalloutLineAnchorCapStyle = LineAnchorCapStyle.Arrow;
+      ds.SmartLabelStyle.CalloutLineColor = System.Drawing.Color.Red;
+      ds.SmartLabelStyle.CalloutLineWidth = 1;
+      ds.SmartLabelStyle.CalloutStyle = LabelCalloutStyle.Underlined;
 
       // Set marker size
       ds.MarkerSize = 15;
 
       // Set marker shape
-      ds.MarkerStyle = MarkerStyle.Diamond;
-
-      // Set to 3D
-      //msChart.GetChart().ChartAreas[strChartArea].Area3DStyle.Enable3D = true;
-
-      msChart.GetChart().Series.Add(ds);
-      //MsSeriesCollection.Add(ds);
+      ds.MarkerStyle = MarkerStyle.Circle;
     }
+
+
+    public void SetupChart(Chart chart, RaceResultViewProvider results)
+    {
+      createAndAddChartArea(msChart.GetChart(), results);
+
+      // Setup Data Series
+      chart.Series.Clear();
+      var ds = new System.Windows.Forms.DataVisualization.Charting.Series();
+      fillDataSeries(ds, results);
+      formatDataSeries(ds, results);
+      msChart.GetChart().Series.Add(ds);
+
+      //msChart.GetChart().SaveImage("", ChartImageFormat.)
+    }
+
+
   }
 }
