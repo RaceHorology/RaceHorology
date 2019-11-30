5e35b510af893c1450fa578ec67e37881c640d44
Sven Flossmann <sven.flossmann@gmail.com>
2019-11-30 14:32:14 +0100

    CHANGED: Chart fine tuning

--- DSVAlpin2Lib/ResultCharts.cs
+++ DSVAlpin2Lib/ResultCharts.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,69 @@ namespace DSVAlpin2Lib
 {
   public class ResultCharts
   {
+
+
+    //Base10Exponent returns the integer exponent (N) that would yield a
+    //number of the form A x Exp10(N), where 1.0 <= |A| < 10.0
+    protected static int base10Exponent(double num)
+    {
+      if (num == 0)
+        return -Int32.MaxValue;
+      else
+        return Convert.ToInt32(Math.Floor(Math.Log10(Math.Abs(num))));
+    }
+
+    double[] roundMantissa = { 1.00d, 1.20d, 1.40d, 1.60d, 1.80d, 2.00d, 2.50d, 3.00d, 4.00d, 5.00d, 6.00d, 8.00d, 10.00d };
+    double[] roundInterval = { 0.20d, 0.20d, 0.20d, 0.20d, 0.20d, 0.50d, 0.50d, 0.50d, 0.50d, 1.00d, 1.00d, 2.00d, 2.00d };
+    double[] roundIntMinor = { 0.05d, 0.05d, 0.05d, 0.05d, 0.05d, 0.10d, 0.10d, 0.10d, 0.10d, 0.20d, 0.20d, 0.50d, 0.50d };
+
+    /// <summary>
+    /// Gets nice round numbers for the axes. For the horizontal axis, minValue is always 0.
+    /// </summary>
+    /// <param name="minValue"></param>
+    /// <param name="maxValue"></param>
+    /// <param name="axisInterval"></param>
+    protected void getNiceRoundNumbers(ref double minValue, ref double maxValue, ref double interval, ref double intMinor)
+    {
+      double min = Math.Min(minValue, maxValue);
+      double max = Math.Max(minValue, maxValue);
+      double delta = max - min; //The full range
+                                //Special handling for zero full range
+      if (delta == 0)
+      {
+        //When min == max == 0, choose arbitrary range of 0 - 1
+        if (min == 0)
+        {
+          minValue = 0;
+          maxValue = 1;
+          interval = 0.2;
+          intMinor = 0.5;
+          return;
+        }
+        //min == max, but not zero, so set one to zero
+        if (min < 0)
+          max = 0; //min-max are -|min| to 0
+        else
+          min = 0; //min-max are 0 to +|max|
+        delta = max - min;
+      }
+
+      int N = base10Exponent(delta);
+      double tenToN = Math.Pow(10, N);
+      double A = delta / tenToN;
+      //At this point delta = A x Exp10(N), where
+      // 1.0 <= A < 10.0 and N = integer exponent value
+      //Now, based on A select a nice round interval and maximum value
+      for (int i = 0; i < roundMantissa.Length; i++)
+        if (A <= roundMantissa[i])
+        {
+          interval = roundInterval[i] * tenToN;
+          intMinor = roundIntMinor[i] * tenToN;
+          break;
+        }
+      minValue = interval * Math.Floor(min / interval);
+      maxValue = interval * Math.Ceiling(max / interval);
+    }
 
 
     public void configureAxis(ChartArea area, RaceResultViewProvider results)
@@ -79,20 +143,34 @@ namespace DSVAlpin2Lib
         }
       }
 
-      timeMin = Math.Floor(timeMin);
-      timeMax = Math.Ceiling(timeMax);
-
       // Enable X axis labels automatic fitting
       area.AxisX.IsLabelAutoFit = true;
       area.AxisX.LabelAutoFitStyle = LabelAutoFitStyles.DecreaseFont | LabelAutoFitStyles.IncreaseFont | LabelAutoFitStyles.WordWrap;
       area.AxisX.MajorGrid.Enabled = false;
       area.AxisX.MinorGrid.Enabled = false;
 
+
+      double yInt, yMinInt;
+      yInt = yMinInt = 0;
+      getNiceRoundNumbers(ref timeMin, ref timeMax, ref yInt, ref yMinInt);
+
+
       area.AxisY.Minimum = timeMin;
       area.AxisY.Maximum = timeMax;
+      area.AxisY.Interval = yInt;
+      area.AxisY.MinorTickMark.Interval = yMinInt;
+      area.AxisY.MinorTickMark.TickMarkStyle = TickMarkStyle.OutsideArea;
+      area.AxisY.MinorTickMark.Enabled = true;
+      area.AxisY.MinorTickMark.Size = area.AxisY.MajorTickMark.Size / 2;
+      
       area.AxisY.IsMarginVisible = false;
       area.AxisY.IsStartedFromZero = false;
-      area.AxisY.Title = "Zeit";
+      area.AxisY.Title = "Zeit [s]";
+      area.AxisY.TitleFont = new System.Drawing.Font("Helvetica", 10);
+      area.AxisY.LabelAutoFitStyle = LabelAutoFitStyles.None;
+      //area.AxisY.LabelStyle.Format = "#.##";
+      //area.AxisY.RoundAxisValues();
+      //area.AxisY.IntervalType = DateTimeIntervalType.= IntervalAutoMode.
 
       area.AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Solid;
       area.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
@@ -105,14 +183,14 @@ namespace DSVAlpin2Lib
 
 
       // Enable scale breaks
-      area.AxisY.ScaleBreakStyle.Enabled = true;
-      area.AxisY.ScaleBreakStyle.BreakLineStyle = BreakLineStyle.Wave;
-      area.AxisY.ScaleBreakStyle.Spacing = 2;
-      area.AxisY.ScaleBreakStyle.LineWidth = 2;
-      area.AxisY.ScaleBreakStyle.LineColor = System.Drawing.Color.Red;
-      area.AxisY.ScaleBreakStyle.CollapsibleSpaceThreshold = 10;
-      // If all data points are significantly far from zero, the Chart will calculate the scale minimum value
-      area.AxisY.ScaleBreakStyle.StartFromZero = StartFromZero.Yes;
+      area.AxisY.ScaleBreakStyle.Enabled = false;
+      //area.AxisY.ScaleBreakStyle.BreakLineStyle = BreakLineStyle.Wave;
+      //area.AxisY.ScaleBreakStyle.Spacing = 2;
+      //area.AxisY.ScaleBreakStyle.LineWidth = 2;
+      //area.AxisY.ScaleBreakStyle.LineColor = System.Drawing.Color.Red;
+      //area.AxisY.ScaleBreakStyle.CollapsibleSpaceThreshold = 10;
+      //// If all data points are significantly far from zero, the Chart will calculate the scale minimum value
+      //area.AxisY.ScaleBreakStyle.StartFromZero = StartFromZero.Yes;
     }
 
 
@@ -130,7 +208,19 @@ namespace DSVAlpin2Lib
         DataPoint p = new DataPoint(x, timeValue);
         //DataPoint p = new DataPoint(timeValue, x);
         //p.Label = item.Participant.Fullname;
-        p.Label = string.Format("{0} {1}.", item.Participant.Name, item.Participant.Firstname.Substring(0, 1));
+        //p.Label = string.Format("{0} {1}.", item.Participant.Name, item.Participant.Firstname.Substring(0, 1));
+        p.Label = string.Format("{0} ({1}{2})", item.Participant.StartNumber, item.Participant.Name.Substring(0, 1), item.Participant.Firstname.Substring(0, 1));
+
+        if (item.Participant.Sex == "M")
+        {
+          p.MarkerColor = System.Drawing.Color.FromArgb(0x26, 0xb5, 0xd9); //93abc6, 26b5d9
+          p.MarkerStyle = MarkerStyle.Circle;
+        }
+        else
+        {
+          p.MarkerColor = System.Drawing.Color.FromArgb(0xf0, 0x4a, 0xea); // c493c6, f04aea
+          p.MarkerStyle = MarkerStyle.Diamond;
+        }
 
         ds.Points.Add(p);
       }
@@ -143,13 +233,17 @@ namespace DSVAlpin2Lib
       {
         ChartArea area = new ChartArea();
         chart.ChartAreas.Add(area);
+
+        area.BorderColor = Color.FromArgb(0x3f, 0x43, 0x4b);
+        area.BorderDashStyle = ChartDashStyle.Solid;
+        area.BorderWidth = 1;
       }
 
       configureAxis(chart.ChartAreas[0], results);
     }
 
 
-    protected void fillDataSeries(System.Windows.Forms.DataVisualization.Charting.Series ds, RaceResultViewProvider results)
+    protected void createDataSerieses(RaceResultViewProvider results, Chart chart)
     {
       // Populate series data with random data
       var lr = results.GetView() as System.Windows.Data.ListCollectionView;
@@ -160,17 +254,33 @@ namespace DSVAlpin2Lib
         {
           if (group is System.Windows.Data.CollectionViewGroup cvGroup)
           {
-            foreach (var result in cvGroup.Items)
-              addResult(ds, result, x);
+            System.Windows.Forms.DataVisualization.Charting.Series ds = new Series(cvGroup.Name.ToString());
 
+            if (cvGroup.Items.Count > 0)
+            {
+              formatDataSeries(ds);
+              fillDataSeries(ds, cvGroup.Items, x);
+            }
             x++;
+
+            chart.Series.Add(ds);
           }
         }
       }
+
+
     }
 
 
-    protected void formatDataSeries(System.Windows.Forms.DataVisualization.Charting.Series ds, RaceResultViewProvider results)
+
+    protected void fillDataSeries(System.Windows.Forms.DataVisualization.Charting.Series ds, System.Collections.IEnumerable items, int xValue)
+    {
+      foreach (var result in items)
+        addResult(ds, result, xValue);
+    }
+
+
+    protected void formatDataSeries(System.Windows.Forms.DataVisualization.Charting.Series ds)
     {
       // Set point chart type
       ds.ChartType = SeriesChartType.Point;
@@ -200,10 +310,7 @@ namespace DSVAlpin2Lib
 
       // Setup Data Series
       chart.Series.Clear();
-      var ds = new System.Windows.Forms.DataVisualization.Charting.Series();
-      fillDataSeries(ds, results);
-      formatDataSeries(ds, results);
-      chart.Series.Add(ds);
+      createDataSerieses(results, chart);
 
       //chart.SaveImage("", ChartImageFormat.)
     }
