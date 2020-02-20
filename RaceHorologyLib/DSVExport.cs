c5e3ca67f046389914524258ff3f57f93bfff066
Sven Flossmann <sven.flossmann@gmail.com>
2020-02-20 22:38:41 +0100

    ADDED: First sketch of exporting data to DSV XML format

---
+++ RaceHorologyLib/DSVExport.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RaceHorologyLib
+{
+  public class DSVExport
+  {
+    protected XmlWriterSettings _xmlSettings;
+    protected XmlWriter _writer;
+
+    public DSVExport()
+    {
+      _xmlSettings = new XmlWriterSettings();
+      _xmlSettings.Indent = true;
+      _xmlSettings.IndentChars = "  ";
+      _xmlSettings.Encoding = Encoding.UTF8;
+      _xmlSettings.NewLineOnAttributes = true;
+    }
+
+    public void Export(string pathXMLFile, Race race)
+    {
+      FileStream output = new FileStream(pathXMLFile, FileMode.Create);
+      Export(output, race);
+    }
+
+
+    public void Export(Stream output, Race race)
+    {
+      _writer = XmlWriter.Create(output, _xmlSettings);
+
+      _writer.WriteStartDocument();
+      writeRace(race);
+      _writer.WriteEndDocument();
+
+      _writer.Close();
+    }
+
+
+    protected void writeRace(Race race)
+    {
+      _writer.WriteStartElement("dsv_alpine_raceresults");
+      writeRaceDescription(race);
+      _writer.WriteEndElement();
+    }
+
+    protected void writeRaceDescription(Race race)
+    {
+      _writer.WriteStartElement("racedescription");
+
+      _writer.WriteStartElement("racedate");
+      _writer.WriteValue(race.DateResult.ToString("yyyy-MM-dd"));
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("gender");
+      _writer.WriteValue("A"); // TODO: get real gender
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("season");
+      _writer.WriteValue(race.DateResult.AddMonths(2).ToString("yyyy"));
+      _writer.WriteEndElement();
+
+      _writer.WriteEndElement();
+
+    }
+
+  }
+}
---
+++ RaceHorologyLibTest/DSVExportTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceHorologyLib;
+using System.IO;
+
+namespace RaceHorologyLibTest
+{
+  /// <summary>
+  /// Summary description for DSVExportTest
+  /// </summary>
+  [TestClass]
+  public class DSVExportTest
+  {
+    public DSVExportTest()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem(@"TestDataBases\FullTestCases\Case1\KSC4--U12.mdb")]
+    [DeploymentItem(@"TestDataBases\FullTestCases\Case1\KSC4--U12_GiantSlalom.config")]
+    [DeploymentItem(@"TestDataBases\FullTestCases\Case1\KSC4--U12_ALGE_Run1.txt")]
+    [DeploymentItem(@"TestDataBases\FullTestCases\Case1\KSC4--U12_ALGE_Run1.txt")]
+    public void Test1()
+    {
+      string dbFilename = TestUtilities.CreateWorkingFileFrom(testContextInstance.TestDeploymentDir, @"KSC4--U12.mdb");
+
+      // Setup Data Model & Co
+      Database db = new Database();
+      db.Connect(dbFilename);
+
+      AppDataModel model = new AppDataModel(db);
+
+      DSVExport dsvExport = new DSVExport();
+
+      MemoryStream xmlData = new MemoryStream();
+      dsvExport.Export(xmlData, model.GetRace(0));
+
+      xmlData.Position = 0;
+      StreamReader reader = new StreamReader(xmlData);
+      string s = reader.ReadToEnd();
+    }
+  }
+}
