e3b0ebafe9e0869c81801587a6ba0bbe51127ab0
Sven Flossmann <sven.flossmann@gmail.com>
2020-02-23 13:33:45 +0100

    CHANGED: Implemented major parts of exporting data to DSV XML format

---
+++ RaceHorologyLib/DSVCalculations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceHorologyLib
+{
+  public class DSVRaceCalculation
+  {
+    private Race _race;
+
+    public DSVRaceCalculation(Race race)
+    {
+      _race = race;
+    }
+
+
+    public double CalculatedPenaltyMen { get; }
+    public double CalculatedPenaltyWomen { get; }
+
+    public double AppliedPenaltyMen { get; set; }
+    public double AppliedPenaltyWomen { get; set; }
+
+  }
+}
--- RaceHorologyLib/DSVExport.cs
+++ RaceHorologyLib/DSVExport.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -19,7 +21,7 @@ namespace RaceHorologyLib
       _xmlSettings.Indent = true;
       _xmlSettings.IndentChars = "  ";
       _xmlSettings.Encoding = Encoding.UTF8;
-      _xmlSettings.NewLineOnAttributes = true;
+      //_xmlSettings.NewLineOnAttributes = true;
     }
 
     public void Export(string pathXMLFile, Race race)
@@ -45,6 +47,8 @@ namespace RaceHorologyLib
     {
       _writer.WriteStartElement("dsv_alpine_raceresults");
       writeRaceDescription(race);
+      writeRaceData(race);
+      writeRaceResults(race);
       _writer.WriteEndElement();
     }
 
@@ -64,9 +68,466 @@ namespace RaceHorologyLib
       _writer.WriteValue(race.DateResult.AddMonths(2).ToString("yyyy"));
       _writer.WriteEndElement();
 
+      _writer.WriteStartElement("raceid");
+      _writer.WriteValue(race.RaceNumber);
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("raceorganizer");
+      _writer.WriteValue(race.AdditionalProperties.Organizer);
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("discipline");
+      _writer.WriteValue(getDSVDisciplin(race));
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("category");
+      _writer.WriteValue(getDSVCategory(race));
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("racename");
+      _writer.WriteValue(race.AdditionalProperties.Name);
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("raceplace");
+      _writer.WriteValue(race.AdditionalProperties.Location);
+      _writer.WriteEndElement();
+
+      _writer.WriteStartElement("timing");
+      _writer.WriteValue("Alge TdC8001"); // TODO: make variable
+      _writer.WriteEndElement();
+
+      if (!string.IsNullOrEmpty(race.AdditionalProperties.Analyzer))
+      {
+        _writer.WriteStartElement("dataprocessing_by");
+        _writer.WriteValue(race.AdditionalProperties.Analyzer);
+        _writer.WriteEndElement();
+      }
+
+      if (getSoftware() != null)
+      {
+        _writer.WriteStartElement("software");
+        _writer.WriteValue(getSoftware());
+        _writer.WriteEndElement();
+      }
+
       _writer.WriteEndElement();
 
     }
 
+    string getDSVDisciplin(Race race)
+    {
+      if (race.RaceType == Race.ERaceType.DownHill)
+        return "DH";
+      if (race.RaceType == Race.ERaceType.GiantSlalom)
+        return "RS";
+      if (race.RaceType == Race.ERaceType.ParallelSlalom)
+        return "PSL";
+      if (race.RaceType == Race.ERaceType.Slalom)
+        return "SL";
+      if (race.RaceType == Race.ERaceType.SuperG)
+        return "SG";
+
+      if (race.RaceType == Race.ERaceType.KOSlalom)
+        return "unknown";
+
+      return "unknown";
+    }
+
+    string getDSVCategory(Race race)
+    {
+      return "unknown";
+    }
+
+
+    string getSoftware()
+    {
+      Assembly assembly = Assembly.GetEntryAssembly();
+      if (assembly == null)
+        assembly = Assembly.GetExecutingAssembly();
+
+      if (assembly != null)
+      {
+        FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+        var productName = fvi.ProductName;
+        var productVersion = fvi.ProductVersion;
+
+        return string.Format("{0} {1}", productName, productVersion);
+      }
+
+      return null;
+    }
+
+    protected void writeRaceData(Race race)
+    {
+      _writer.WriteStartElement("racedata");
+
+      _writer.WriteStartElement("useddsvlist");
+      _writer.WriteValue("unknown"); // TODO: needs to be fixed
+      _writer.WriteEndElement();
+
+      if (false) // TODO: needs to be fixed, non-optional
+      {
+        _writer.WriteStartElement("fvalue");
+        _writer.WriteValue("unknown");
+        _writer.WriteEndElement();
+      }
+
+      if (false) // TODO: needs to be fixed, btw: optional
+      {
+        DSVRaceCalculation dsvCalc = new DSVRaceCalculation(race);
+        // women
+        _writer.WriteStartElement("racepenalty");
+        _writer.WriteAttributeString("gender", "L");
+
+        _writer.WriteStartElement("applied_penalty");
+        _writer.WriteValue(dsvCalc.AppliedPenaltyWomen);
+        _writer.WriteEndElement();
+
+        _writer.WriteStartElement("calculated_penalty");
+        _writer.WriteValue(dsvCalc.CalculatedPenaltyWomen);
+        _writer.WriteEndElement();
+
+        _writer.WriteEndElement();
+
+        // men
+        _writer.WriteStartElement("racepenalty");
+        _writer.WriteAttributeString("gender", "M");
+
+        _writer.WriteStartElement("applied_penalty");
+        _writer.WriteValue(dsvCalc.AppliedPenaltyMen);
+        _writer.WriteEndElement();
+
+        _writer.WriteStartElement("calculated_penalty");
+        _writer.WriteValue(dsvCalc.CalculatedPenaltyMen);
+        _writer.WriteEndElement();
+
+        _writer.WriteEndElement();
+      }
+
+      writeJuryPerson(_writer, "ChiefRace", race.AdditionalProperties.RaceManager);
+      writeJuryPerson(_writer, "Referee", race.AdditionalProperties.RaceReferee);
+      writeJuryPerson(_writer, "RepresentativeTrainer", race.AdditionalProperties.TrainerRepresentative);
+
+      writeRunData(race.GetRun(0), race.AdditionalProperties.RaceRun1);
+      if (race.GetMaxRun() > 1)
+        writeRunData(race.GetRun(1), race.AdditionalProperties.RaceRun2);
+
+      _writer.WriteEndElement();
+    }
+
+
+    static void writeJuryPerson(XmlWriter writer, string function, AdditionalRaceProperties.Person person)
+    {
+      writer.WriteStartElement("racejury");
+      writer.WriteAttributeString("function", function);
+
+      writePersonData(writer, person);
+
+      writer.WriteEndElement();
+    }
+
+    static void writeCourseSetterPerson(XmlWriter writer, AdditionalRaceProperties.Person person)
+    {
+      writer.WriteStartElement("coursesetter");
+      writePersonData(writer, person);
+      writer.WriteEndElement();
+    }
+
+    static void writeForeRunnerPerson(XmlWriter writer, int frNo, AdditionalRaceProperties.Person person)
+    {
+      writer.WriteStartElement("forerunner");
+      writer.WriteAttributeString("order", frNo.ToString());
+
+      writePersonData(writer, person);
+
+      writer.WriteEndElement();
+    }
+
+    static void writePersonData(XmlWriter writer, AdditionalRaceProperties.Person person)
+    {
+      writer.WriteStartElement("lastname");
+      writer.WriteValue(person.Name);
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("club");
+      writer.WriteValue(person.Club);
+      writer.WriteEndElement();
+    }
+
+
+    void writeRunData(RaceRun raceRun, AdditionalRaceProperties.RaceRunProperties raceRunProperties)
+    {
+      _writer.WriteStartElement("rundata");
+      _writer.WriteAttributeString("runnumber", raceRun.Run.ToString());
+
+      {
+        _writer.WriteStartElement("coursedata");
+
+        _writer.WriteStartElement("coursename");
+        _writer.WriteValue(raceRun.GetRace().AdditionalProperties.CoarseName);
+        _writer.WriteEndElement();
+
+        if (!string.IsNullOrEmpty(raceRun.GetRace().AdditionalProperties.CoarseHomologNo))
+        {
+          _writer.WriteStartElement("homologationnumber");
+          _writer.WriteValue(raceRun.GetRace().AdditionalProperties.CoarseHomologNo);
+          _writer.WriteEndElement();
+        }
+
+        _writer.WriteStartElement("number_of_gates");
+        _writer.WriteValue(raceRunProperties.Gates.ToString());
+        _writer.WriteEndElement();
+
+        _writer.WriteStartElement("number_of_turninggates");
+        _writer.WriteValue(raceRunProperties.Turns.ToString());
+        _writer.WriteEndElement();
+
+        _writer.WriteStartElement("startaltitude");
+        _writer.WriteValue(raceRun.GetRace().AdditionalProperties.StartHeight.ToString());
+        _writer.WriteEndElement();
+
+        _writer.WriteStartElement("finishaltitude");
+        _writer.WriteValue(raceRun.GetRace().AdditionalProperties.StartHeight.ToString());
+        _writer.WriteEndElement();
+
+        _writer.WriteStartElement("courselength");
+        _writer.WriteValue(raceRun.GetRace().AdditionalProperties.CoarseLength.ToString());
+        _writer.WriteEndElement();
+
+        writeCourseSetterPerson(_writer, raceRunProperties.CoarseSetter);
+
+        if (!string.IsNullOrWhiteSpace(raceRunProperties.Forerunner1.Name))
+          writeForeRunnerPerson(_writer, 1, raceRunProperties.Forerunner1);
+        if (!string.IsNullOrWhiteSpace(raceRunProperties.Forerunner2.Name))
+          writeForeRunnerPerson(_writer, 2, raceRunProperties.Forerunner2);
+        if (!string.IsNullOrWhiteSpace(raceRunProperties.Forerunner3.Name))
+          writeForeRunnerPerson(_writer, 3, raceRunProperties.Forerunner3);
+
+        _writer.WriteEndElement();
+      }
+
+      {
+        _writer.WriteStartElement("meteodata");
+
+        if (string.IsNullOrEmpty(raceRun.GetRace().AdditionalProperties.Weather))
+        {
+          _writer.WriteStartElement("weather");
+          _writer.WriteValue(raceRun.GetRace().AdditionalProperties.Weather);
+          _writer.WriteEndElement();
+        }
+        if (string.IsNullOrEmpty(raceRun.GetRace().AdditionalProperties.Snow))
+        {
+          _writer.WriteStartElement("snowtexture");
+          _writer.WriteValue(raceRun.GetRace().AdditionalProperties.Snow);
+          _writer.WriteEndElement();
+        }
+        if (string.IsNullOrEmpty(raceRun.GetRace().AdditionalProperties.TempStart))
+        {
+          _writer.WriteStartElement("temperature_startaltitude");
+          _writer.WriteValue(raceRun.GetRace().AdditionalProperties.TempStart);
+          _writer.WriteEndElement();
+        }
+        if (string.IsNullOrEmpty(raceRun.GetRace().AdditionalProperties.TempFinish))
+        {
+          _writer.WriteStartElement("temperature_finishaltitude");
+          _writer.WriteValue(raceRun.GetRace().AdditionalProperties.TempFinish);
+          _writer.WriteEndElement();
+        }
+
+        _writer.WriteEndElement();
+      }
+
+      {
+        _writer.WriteStartElement("starttime");
+        _writer.WriteValue(raceRunProperties.StartTime); // TODO: Ensure "HH:MM" 24h
+        _writer.WriteEndElement();
+      }
+
+      _writer.WriteEndElement();
+    }
+
+
+    void writeRaceResults(Race race)
+    {
+      // Step 1: split according to classified or not classified
+      List<RaceResultItem> classified = new List<RaceResultItem>();
+      List<RaceResultItem> notClassified = new List<RaceResultItem>();
+
+      var results = race.GetResultViewProvider().GetView();
+      var lr = results as System.Windows.Data.ListCollectionView;
+
+      foreach (var result in results.SourceCollection)
+      {
+        RaceResultItem item = result as RaceResultItem;
+
+        if (item.ResultCode == RunResult.EResultCode.Normal)
+          classified.Add(item);
+        else
+          notClassified.Add(item);
+      }
+
+      writeClassifiedCompetitors(classified);
+      writeNotClassifiedCompetitors(notClassified);
+
+    }
+
+    void writeClassifiedCompetitors(List<RaceResultItem> classified)
+    {
+      _writer.WriteStartElement("classified_competitors");
+
+      foreach (RaceResultItem rri in classified)
+      {
+        _writer.WriteStartElement("ranked");
+        _writer.WriteAttributeString("gender", mapSex(rri.Participant));
+        _writer.WriteAttributeString("status", "QLF");
+        _writer.WriteAttributeString("rank", rri.Position.ToString());
+        _writer.WriteAttributeString("bib", rri.Participant.StartNumber.ToString());
+
+        writeCompetitor(_writer, rri.Participant);
+        writeRaceResult(_writer, rri);
+
+        _writer.WriteEndElement();
+      }
+
+      _writer.WriteEndElement();
+    }
+
+    void writeNotClassifiedCompetitors(List<RaceResultItem> notClassified)
+    {
+      _writer.WriteStartElement("not_classified_competitors");
+
+      foreach (RaceResultItem rri in notClassified)
+      {
+        _writer.WriteStartElement("notranked");
+        _writer.WriteAttributeString("gender", mapSex(rri.Participant));
+        _writer.WriteAttributeString("bib", rri.Participant.StartNumber.ToString());
+        _writer.WriteAttributeString("status", mapResultCode(rri.ResultCode));
+
+        if (!string.IsNullOrWhiteSpace(rri.DisqualText))
+        {
+          _writer.WriteStartElement("notranked");
+          _writer.WriteValue(rri.DisqualText);
+          _writer.WriteEndElement();
+        }
+
+        writeCompetitor(_writer, rri.Participant);
+        writeRaceResult(_writer, rri);
+
+        _writer.WriteEndElement();
+      }
+
+      _writer.WriteEndElement();
+    }
+
+
+    static void writeCompetitor(XmlWriter writer, RaceParticipant participant)
+    {
+      writer.WriteStartElement("competitor");
+
+      writer.WriteStartElement("dsvcode");
+      writer.WriteValue(participant.Participant.CodeOrSvId);
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("year_of_birth");
+      writer.WriteValue(participant.Participant.Year);
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("gender");
+      writer.WriteValue(mapSex(participant));
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("lastname");
+      writer.WriteValue(participant.Name);
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("firstname");
+      writer.WriteValue(participant.Firstname);
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("club");
+      writer.WriteValue(participant.Club);
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("association"); // TODO: currently Verband = Nation 
+      writer.WriteValue(participant.Nation);
+      writer.WriteEndElement();
+
+      writer.WriteEndElement();
+    }
+
+    static void writeRaceResult(XmlWriter writer, RaceResultItem rri)
+    {
+      writer.WriteStartElement("raceresult");
+
+      writer.WriteStartElement("dsvlistpoints");
+      writer.WriteValue(rri.Participant.Points);
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("totaltime");
+      writer.WriteValue(formatTime(rri.TotalTime));
+      writer.WriteEndElement();
+
+      writer.WriteStartElement("racepoints");
+      writer.WriteValue(rri.Points);
+      writer.WriteEndElement();
+
+      foreach (var x in rri.SubResults)
+      {
+        writer.WriteStartElement("runtime");
+        writer.WriteAttributeString("runnumber", x.Key.ToString());
+
+        writer.WriteStartElement("time_of_run");
+        writer.WriteValue(formatTime(x.Value.Runtime));
+        writer.WriteEndElement();
+
+        writer.WriteEndElement();
+      }
+
+
+      writer.WriteEndElement();
+    }
+
+    static string mapResultCode(RunResult.EResultCode resultCode)
+    {
+      switch (resultCode)
+      {
+        case RunResult.EResultCode.DIS:
+          return "DSQ";
+        case RunResult.EResultCode.NaS:
+          return "DNS";
+        case RunResult.EResultCode.NiZ:
+          return "DNF";
+        case RunResult.EResultCode.NQ:
+          return "DNQ";
+      }
+
+      return "UNKNOWN";
+    }
+
+
+    static string mapSex(RaceParticipant particpant)
+    {
+      switch (particpant.Sex)
+      {
+        case "M":
+        case "m":
+        case "H":
+        case "h":
+          return "M";
+        case "W":
+        case "w":
+        case "D":
+        case "d":
+          return "L";
+      }
+
+      return "U";
+    }
+
+
+    static string formatTime(TimeSpan? time)
+    {
+      return time?.ToString(@"mm\:ss\.ff");
+    }
   }
 }
