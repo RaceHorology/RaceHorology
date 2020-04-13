11411b8eadc719357636137e5247eedf253a5e99
Sven Flossmann <sven.flossmann@gmail.com>
2020-04-13 21:34:23 +0200

    ADDED: Initial structure for reading CSV/Excel

---
+++ RaceHorologyLib/Import.cs
@@ -0,0 +1,121 @@
+﻿/*
+ *  Copyright (C) 2019 - 2020 by Sven Flossmann
+ *  
+ *  This file is part of Race Horology.
+ *
+ *  Race Horology is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ * 
+ *  Race Horology is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Race Horology.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Diese Datei ist Teil von Race Horology.
+ *
+ *  Race Horology ist Freie Software: Sie können es unter den Bedingungen
+ *  der GNU Affero General Public License, wie von der Free Software Foundation,
+ *  Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+ *  veröffentlichten Version, weiter verteilen und/oder modifizieren.
+ *
+ *  Race Horology wird in der Hoffnung, dass es nützlich sein wird, aber
+ *  OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ *  Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ *  Siehe die GNU Affero General Public License für weitere Details.
+ *
+ *  Sie sollten eine Kopie der GNU Affero General Public License zusammen mit diesem
+ *  Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceHorologyLib
+{
+
+  public class ImportReader
+  {
+    DataSet _dataSet;
+
+    public ImportReader(string path)
+    {
+      IExcelDataReader reader;
+      if (System.IO.Path.GetExtension(path).ToLower() == ".csv")
+      {
+        // CSV File
+        var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+        reader = ExcelReaderFactory.CreateCsvReader(stream);
+        _dataSet = reader.AsDataSet(new ExcelDataSetConfiguration() { ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration() { UseHeaderRow = true } });
+      }
+      else
+      {
+        // Excel
+        var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+        reader = ExcelReaderFactory.CreateReader(stream);
+        _dataSet = reader.AsDataSet();
+      }
+
+      Columns = extractFields(_dataSet);
+    }
+
+    public DataSet Data { get { return _dataSet; } }
+
+    public List<string> Columns { get; set; }
+
+    protected static List<string> extractFields(DataSet ds)
+    {
+      List<string> ret = new List<string>();
+      foreach (DataColumn col in ds.Tables[0].Columns)
+      {
+        ret.Add(col.ColumnName);
+      }
+      return ret;
+    }
+  }
+
+
+  public class Mapping
+  {
+    Dictionary<string, string> _mapping; // Required, Provided
+    List<string> _availableFields;
+    List<string> _requiredFields;
+
+    public Mapping(List<string>requiredFields, List<string>availableFields)
+    {
+      _availableFields = availableFields;
+      _requiredFields = requiredFields;
+
+      _mapping = new Dictionary<string, string>();
+    }
+
+
+    public void Assign(string requiredField, string providedField)
+    {
+      _mapping[requiredField] = providedField;
+    }
+
+    public Dictionary<string, string> MappingList { get { return _mapping; } private set { _mapping = value; } }
+
+
+
+  }
+
+
+  class Import
+  {
+    public Import(DataSet ds, List<Participant> particpants, Mapping mapping)
+    { }
+  }
+}
---
+++ RaceHorologyLibTest/ImportTest.cs
@@ -0,0 +1,78 @@
+﻿using RaceHorologyLib;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace RaceHorologyLibTest
+{
+  /// <summary>
+  /// Summary description for ImportTest
+  /// </summary>
+  [TestClass]
+  public class ImportTest
+  {
+    public ImportTest()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+    }
+
+    [TestMethod]
+    public void DetectColumn()
+    {
+      //var stream = File.Open(@"C:\Users\sven\Dropbox\SyncFolder\WSVGlonn_Zeitnahme\Rennen 2020\2020-02-02 - KC4\Bewerbsdaten\Anmeldung\Teilnehmer_V1_202001301844.xls", FileMode.Open, FileAccess.Read);
+
+      var ir = new ImportReader(@"C:\Users\sven\Dropbox\SyncFolder\WSVGlonn_Zeitnahme\Rennen 2020\2020-02-02 - KC4\Bewerbsdaten\Anmeldung\Teilnehmer_V1_202001301844-U14-.csv");
+      var columns = ir.Columns;
+    }
+  }
+}
