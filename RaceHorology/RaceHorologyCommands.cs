442a8a1695763f84ecc7701baeeca135b3c4477a
Sven Flossmann <sven.flossmann@gmail.com>
2020-10-04 20:48:47 +0200

    ADDED: Customized commands for RaceHorlogy (to be used in menus)

---
+++ RaceHorology/RaceHorologyCommands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace RaceHorology.Commands
+{
+	public static class RaceHorologyCommands
+	{
+		public static readonly RoutedUICommand Exit = new RoutedUICommand
+			(
+				"Exit",
+				"Exit",
+				typeof(RaceHorologyCommands),
+				new InputGestureCollection()
+				{
+					new KeyGesture(Key.F4, ModifierKeys.Alt)
+				}
+			);
+
+		public static readonly RoutedUICommand HandTime = new RoutedUICommand
+			(
+				"HandTime",
+				"HandTime",
+				typeof(RaceHorologyCommands)
+			);
+
+		//Define more commands here, just like the one above
+	}
+}
+
+
