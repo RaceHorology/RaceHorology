6b468a1f80246cd440c62083bf43ccac3e5136b5
Sven Flossmann <sven.flossmann@gmail.com>
2020-10-09 20:02:35 +0200

    ADDED: FromFileHandTiming to read hand time from file

--- RaceHorologyLib/OfflineTimingMeasurement.cs
+++ RaceHorologyLib/OfflineTimingMeasurement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,74 @@ namespace RaceHorologyLib
   {
     IEnumerable<TimingData> TimingData();
   }
+
+
+  public class FromFileHandTiming : IHandTiming
+  {
+    string _filePath;
+    StreamReader _file;
+    FromFileParser _parser;
+
+    public FromFileHandTiming(string path)
+    {
+      _filePath = path;
+
+      _parser = new FromFileParser();
+    }
+
+    public void Connect()
+    {
+      _file = new System.IO.StreamReader(_filePath);
+    }
+
+    public void Disconnect()
+    {
+      _file.Close();
+    }
+
+
+    public IEnumerable<TimingData> TimingData()
+    {
+      string line;
+      while ((line = _file.ReadLine()) != null)
+      {
+        var time = _parser.ParseTime(line);
+
+        yield return new TimingData { Time = time };
+      }
+    }
+
+
+  }
+
+
+
+  public class FromFileParser
+  {
+
+    // Format: 09:37:16,40
+    public TimeSpan ParseTime(string line)
+    {
+      TimeSpan parsedTime;
+
+      try
+      {
+        string[] formats = 
+        {
+          @"hh\:mm\:ss\.fffff", @"hh\:mm\:ss\.ffff", @"hh\:mm\:ss\.fff", @"hh\:mm\:ss\.ff", @"hh\:mm\:ss\.f",
+          @"hh\:mm\:ss\,fffff", @"hh\:mm\:ss\,ffff", @"hh\:mm\:ss\,fff", @"hh\:mm\:ss\,ff", @"hh\:mm\:ss\,f"
+        };
+
+        parsedTime = TimeSpan.ParseExact(line, formats, System.Globalization.CultureInfo.InvariantCulture);
+      }
+      catch (FormatException)
+      {
+        throw;
+      }
+
+      return parsedTime;
+    }
+  }
+
 
 }
---
+++ RaceHorologyLibTest/HandTimingTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceHorologyLib;
+
+namespace RaceHorologyLibTest
+{
+  /// <summary>
+  /// Summary description for HandTimingTest
+  /// </summary>
+  [TestClass]
+  public class HandTimingTest
+  {
+    public HandTimingTest()
+    {
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem(@"TestDataBases\HandTime\--Handzeit-Start.txt")]
+    public void ReadFromFile()
+    {
+
+      FromFileHandTiming ht = new FromFileHandTiming(@"--Handzeit-Start.txt");
+      ht.Connect();
+
+      TimeSpan[] shallTime =
+      {
+        new TimeSpan(0, 8, 48, 0, 570),
+        new TimeSpan(0, 9, 32, 56, 300)
+      };
+
+      int i = 0;
+      foreach (var t in ht.TimingData())
+      {
+        if (i < shallTime.Length)
+          Assert.AreEqual(shallTime[i], t.Time);
+
+        TestContext.WriteLine(t.Time.ToString());
+
+        i++;
+      }
+    }
+  }
+}
