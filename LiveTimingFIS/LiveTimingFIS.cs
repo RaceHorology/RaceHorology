bb5435f29e85feeec87c1227b6a73e53c9bd56fc
Sven Flossmann <sven.flossmann@gmail.com>
2021-09-15 20:08:14 +0200

    Outline implementation of LiveTimingFIS

--- LiveTimingFIS/LiveTimingFIS.cs
+++ LiveTimingFIS/LiveTimingFIS.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RaceHorologyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,32 @@ using System.Threading.Tasks;
 
 namespace LiveTimingFIS
 {
-  public class LiveTimingFIS
+  public class LiveTimingFIS : ILiveTiming
   {
+
+    public LiveTimingFIS()
+    { }
+
+    private Race _race;
+    public Race Race
+    {
+      set { _race = value; }
+      get { return _race; }
+    }
+
+    public void Login(string fisRaceCode, string fisCategory, string fisPassword, string fisPort)
+    { 
+    }
+
+    public void Start()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+      throw new NotImplementedException();
+    }
 
   }
 }
