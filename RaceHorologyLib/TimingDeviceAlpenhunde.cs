96ed2f0abdd846ba727810ea4ada1102453e1428
Sven Flossmann <sven.flossmann@sqior.com>
2022-07-11 18:43:16 +0200

    Add class skeleton for Alpenhunde Timing

---
+++ RaceHorologyLib/TimingDeviceAlpenhunde.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceHorologyLib
+{
+  class TimingDeviceAlpenhunde : ILiveTimeMeasurementDevice, ILiveDateTimeProvider
+  {
+    public bool IsOnline => throw new NotImplementedException();
+
+    public event TimeMeasurementEventHandler TimeMeasurementReceived;
+    public event StartnumberSelectedEventHandler StartnumberSelectedReceived;
+    public event LiveTimingMeasurementDeviceStatusEventHandler StatusChanged;
+    public event LiveDateTimeChangedHandler LiveDateTimeChanged;
+
+    public TimeSpan GetCurrentDayTime()
+    {
+      throw new NotImplementedException();
+    }
+
+    public string GetDeviceInfo()
+    {
+      throw new NotImplementedException();
+    }
+
+    public string GetStatusInfo()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
