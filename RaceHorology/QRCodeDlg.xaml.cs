2be73fd694f04893223d64c3a9fd79b81c6b3c03
Sven Flossmann <sven.flossmann@sqior.com>
2022-09-11 18:42:41 +0200

    Add QR Code dialog

---
+++ RaceHorology/QRCodeDlg.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RaceHorology
+{
+  /// <summary>
+  /// Interaction logic for QRCodeDlg.xaml
+  /// </summary>
+  public partial class QRCodeDlg : Window
+  {
+    public QRCodeDlg()
+    {
+      InitializeComponent();
+    }
+  }
+}
