d077ce9324d47e578fc132ab213dacc3c1814a89
Sven Flossmann <sven.flossmann@gmail.com>
2023-02-04 13:34:17 +0100

    Set diff thresholds to 0 - "zero"

--- RaceHorologyLibTest/HandTimingTest.cs
+++ RaceHorologyLibTest/HandTimingTest.cs
@@ -541,7 +541,7 @@ namespace RaceHorologyLibTest
       HandTimingCalc hc = new HandTimingCalc(htVM.Items[5], htVM.Items);
 
       IPDFReport report = new HandTimingCalcReport(hc, tg.Model.GetRace(0));
-      Assert.IsTrue(TestUtilities.GenerateAndCompareAgainstPdf(TestContext, report, @"HandTimingCalc_Report.pdf", 100));
+      Assert.IsTrue(TestUtilities.GenerateAndCompareAgainstPdf(TestContext, report, @"HandTimingCalc_Report.pdf", 0));
     }
 
 
