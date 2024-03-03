cf367c7edd3321ef1678e1908e2cc4011d60ada1
mstenz <mstenz-design@web.de>
2024-03-03 21:21:22 +0100

    enable intelligent textbox selection method for all textboxes in the application

--- RaceHorology/App.xaml.cs
+++ RaceHorology/App.xaml.cs
@@ -40,6 +40,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Controls;
 
 namespace RaceHorology
 {
@@ -50,6 +53,45 @@ namespace RaceHorology
   {
     public App()
     {
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      // Select the text in a TextBox when it receives focus.
+      EventManager.RegisterClassHandler(typeof(TextBox), TextBox.PreviewMouseLeftButtonDownEvent,
+          new MouseButtonEventHandler(SelectivelyIgnoreMouseButton));
+      EventManager.RegisterClassHandler(typeof(TextBox), TextBox.GotKeyboardFocusEvent,
+          new RoutedEventHandler(SelectAllText));
+      EventManager.RegisterClassHandler(typeof(TextBox), TextBox.MouseDoubleClickEvent,
+          new RoutedEventHandler(SelectAllText));
+      base.OnStartup(e);
+    }
+
+    void SelectivelyIgnoreMouseButton(object sender, MouseButtonEventArgs e)
+    {
+      // Find the TextBox
+      DependencyObject parent = e.OriginalSource as UIElement;
+      while (parent != null && !(parent is TextBox))
+        parent = VisualTreeHelper.GetParent(parent);
+
+      if (parent != null)
+      {
+        var textBox = (TextBox)parent;
+        if (!textBox.IsKeyboardFocusWithin)
+        {
+          // If the text box is not yet focused, give it the focus and
+          // stop further processing of this click event.
+          textBox.Focus();
+          e.Handled = true;
+        }
+      }
+    }
+
+    void SelectAllText(object sender, RoutedEventArgs e)
+    {
+      var textBox = e.OriginalSource as TextBox;
+      if (textBox != null)
+        textBox.SelectAll();
     }
   }
 }
