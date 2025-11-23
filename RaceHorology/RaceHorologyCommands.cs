4b076302134c561e26593a4bf0a89dbe65b54540
Sven Flossmann <sven@flossmann.net>
2025-11-23 11:34:30 +0100

    Formatting

--- RaceHorology/RaceHorologyCommands.cs
+++ RaceHorology/RaceHorologyCommands.cs
@@ -1,73 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 
 namespace RaceHorology.Commands
 {
-	public static class RaceHorologyCommands
-	{
-		public static readonly RoutedUICommand Exit = new RoutedUICommand
-			(
-				"Exit",
-				"Exit",
-				typeof(RaceHorologyCommands),
-				new InputGestureCollection()
-				{
-					new KeyGesture(Key.F4, ModifierKeys.Alt)
-				}
-			);
+  public static class RaceHorologyCommands
+  {
+    public static readonly RoutedUICommand Exit = new RoutedUICommand
+    (
+      "Exit",
+      "Exit",
+      typeof(RaceHorologyCommands),
+      new InputGestureCollection()
+      {
+        new KeyGesture(Key.F4, ModifierKeys.Alt)
+      }
+    );
 
-		public static readonly RoutedUICommand HandTime = new RoutedUICommand
-			(
-				"HandTime",
-				"HandTime",
-				typeof(RaceHorologyCommands)
-			);
+    public static readonly RoutedUICommand HandTime = new RoutedUICommand
+    (
+      "HandTime",
+      "HandTime",
+      typeof(RaceHorologyCommands)
+    );
 
-        public static readonly RoutedUICommand CertDesigner = new RoutedUICommand
-			(
-				"CertDesigner",
-				"CertDesigner",
-				typeof(RaceHorologyCommands)
-			);
+    public static readonly RoutedUICommand CertDesigner = new RoutedUICommand
+    (
+      "CertDesigner",
+      "CertDesigner",
+      typeof(RaceHorologyCommands)
+    );
 
 
-        public static readonly RoutedUICommand ImportTime = new RoutedUICommand
-			(
-				"ImportTime",
-				"ImportTime",
-				typeof(RaceHorologyCommands)
-			);
+    public static readonly RoutedUICommand ImportTime = new RoutedUICommand
+    (
+      "ImportTime",
+      "ImportTime",
+      typeof(RaceHorologyCommands)
+    );
 
-		public static readonly RoutedUICommand DeleteRunResults = new RoutedUICommand
-			(
-				"DeleteRunResults",
-				"DeleteRunResults",
-				typeof(RaceHorologyCommands)
-			);
+    public static readonly RoutedUICommand DeleteRunResults = new RoutedUICommand
+    (
+      "DeleteRunResults",
+      "DeleteRunResults",
+      typeof(RaceHorologyCommands)
+    );
 
     public static readonly RoutedUICommand Documentation = new RoutedUICommand
-      (
-        "Documentation",
-        "Documentation",
-        typeof(RaceHorologyCommands)
-      );
+    (
+      "Documentation",
+      "Documentation",
+      typeof(RaceHorologyCommands)
+    );
 
     public static readonly RoutedUICommand AutoUpdate = new RoutedUICommand
-      (
-        "AutoUpdate",
-        "AutoUpdate",
-        typeof(RaceHorologyCommands)
-      );
+    (
+      "AutoUpdate",
+      "AutoUpdate",
+      typeof(RaceHorologyCommands)
+    );
 
     public static readonly RoutedUICommand WA = new RoutedUICommand
-      (
-        "WA",
-        "WA",
-        typeof(RaceHorologyCommands)
-      );
+    (
+      "WA",
+      "WA",
+      typeof(RaceHorologyCommands)
+    );
 
     //Define more commands here, just like the one above
   }
