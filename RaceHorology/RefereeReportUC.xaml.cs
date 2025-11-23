576a4a367c743a2bb7954b25c7ce2847707706d4
Sven Flossmann <sven@flossmann.net>
2025-11-23 11:39:15 +0100

    Formatting

--- RaceHorology/RefereeReportUC.xaml.cs
+++ RaceHorology/RefereeReportUC.xaml.cs
@@ -1,53 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RaceHorologyLib;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using RaceHorologyLib;
 
 namespace RaceHorology
 {
-    /// <summary>
-    /// Interaktionslogik für RefereeReportUC.xaml
-    /// </summary>
-    public partial class RefereeReportUC : UserControl
+  /// <summary>
+  /// Interaktionslogik für RefereeReportUC.xaml
+  /// </summary>
+  public partial class RefereeReportUC : UserControl
+  {
+    public RefereeReportItems ReportItems { get; set; }
+    private Race _race;
+
+    public RefereeReportUC()
     {
-        public RefereeReportItems ReportItems { get; set; }
-        private Race _race;
-
-
-        public RefereeReportUC()
-        {
-            InitializeComponent();
-
-
-    
-        }
-
-        public void Init(Race race)
-        {
-            _race = race;
-            ReportItems = new RefereeReportItems(_race);
-
-            this.DataContext = ReportItems;
-        }
-
-        private void btnApply_Click(object sender, RoutedEventArgs e)
-        {
-            ReportItems.updateList(_race);
-        }
-
-        private void btnReset_Click(object sender, RoutedEventArgs e)
-        {
-        }
+      InitializeComponent();
     }
+
+    public void Init(Race race)
+    {
+      _race = race;
+      ReportItems = new RefereeReportItems(_race);
+      this.DataContext = ReportItems;
+    }
+
+    private void btnApply_Click(object sender, RoutedEventArgs e)
+    {
+      ReportItems.updateList(_race);
+    }
+
+    private void btnReset_Click(object sender, RoutedEventArgs e)
+    {
+    }
+  }
 }
